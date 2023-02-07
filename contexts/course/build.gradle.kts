bb9b15f654b1b7681ae91bb3d283ecb53f8225be
Alejandro Ayala <mayalaat@pichincha.com>
2023-02-07 17:31:59 -0500

    Adding unit test to course domain model

--- contexts/course/build.gradle.kts
+++ contexts/course/build.gradle.kts
@@ -9,6 +9,22 @@ repositories {
     mavenCentral()
 }
 
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // Spotless
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.9.0")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("io.mockk:mockk:1.12.5")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "17"
 }
---
+++ contexts/course/src/test/kotlin/ec/solmedia/course/application/CourseCreatorTest.kt
@@ -0,0 +1,48 @@
+package ec.solmedia.course.application
+
+import ec.solmedia.course.domain.CourseRepository
+import ec.solmedia.course.domain.InvalidCourseId
+import ec.solmedia.course.domain.InvalidCourseName
+import io.mockk.*
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(MockKExtension::class)
+class CourseCreatorTest {
+
+    @MockK(relaxUnitFun = true)
+    private lateinit var repository: CourseRepository
+
+    @InjectMockKs
+    private lateinit var courseCreator: CourseCreator
+
+    companion object {
+        private const val id = "caebae03-3ee9-4aef-b041-21a400fa1bb7"
+        private const val name = "Kotlin Hexagonal Architecture Api Course"
+    }
+
+    @Test
+    fun `should create a course successfully`() {
+        courseCreator.create(id, name)
+
+        verify(exactly = 1) { repository.saveCourse(any()) }
+    }
+
+    @Test
+    fun `should not create a course with invalid id`() {
+        assertThrows<InvalidCourseId> { courseCreator.create("", name) }
+
+        verify(exactly = 0) { repository.saveCourse(any()) }
+    }
+
+    @Test
+    fun `should not create a course with invalid nane`() {
+        assertThrows<InvalidCourseName> { courseCreator.create(id, "   ") }
+
+        verify(exactly = 0) { repository.saveCourse(any()) }
+    }
+}
