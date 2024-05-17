f83eda38ba9f3958a93945f7c10c7b6aea20eb88
Alejandro Ayala Ati <alejandro.ayala@solmedia.ec>
2024-05-17 18:25:45 -0500

    Changing test container configuration

--- src/test/kotlin/ec/solmedia/course/infrastructure/shared/BaseIntegrationTest.kt
+++ src/test/kotlin/ec/solmedia/course/infrastructure/shared/BaseIntegrationTest.kt
@@ -2,36 +2,34 @@ package ec.solmedia.course.infrastructure.shared
 
 import ec.solmedia.shared.Application
 import io.mockk.unmockkAll
-import jakarta.annotation.PostConstruct
-import jakarta.annotation.PreDestroy
 import org.junit.jupiter.api.AfterEach
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 
-@SpringBootTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@ContextConfiguration(classes = [Application::class])
+@JdbcTest
+@Testcontainers
 @ActiveProfiles("test")
+@ContextConfiguration(classes = [Application::class])
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class BaseIntegrationTest {
 
     companion object {
-        @JvmStatic
-        val postgresqlContainer: PostgreSQLContainer<*> = PostgreSQLContainer("postgres:14.5")
-            .withDatabaseName("course_database")
-            .withUsername("course_username")
-            .withPassword("course_password")
+        @Container
+        private val postgresqlContainer: PostgreSQLContainer<*> =
+            PostgreSQLContainer("postgres:14.5")
 
-        @PostConstruct
-        fun start() {
-            postgresqlContainer.start()
-        }
-
-        @PreDestroy
-        fun stop() {
-            postgresqlContainer.stop()
+        @DynamicPropertySource
+        fun properties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url", postgresqlContainer::getJdbcUrl)
+            registry.add("spring.datasource.username", postgresqlContainer::getUsername)
+            registry.add("spring.datasource.password", postgresqlContainer::getPassword)
         }
     }
 
